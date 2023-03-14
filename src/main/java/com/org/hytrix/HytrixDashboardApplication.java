3eff5f64d2c1de6e83732c682342a5cb0733cfb8
srinugithub9676 <githubsolution@gmail.com>
2023-03-14 13:27:18 +0530

    hystrix dashboard

---
+++ src/main/java/com/org/hytrix/HytrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.org.hytrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HytrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HytrixDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/hytrix/HytrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.hytrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HytrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
